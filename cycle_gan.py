d4b37108eb5d6bfa51a960ad012817368f49bea5
burak <byrkbrk@gmail.com>
2024-04-12 23:11:53 +0300

    add optimizers and disc-loss

--- cycle_gan.py
+++ cycle_gan.py
@@ -1,11 +1,13 @@
 import torch
 import torch.nn as nn
+from torch import optim
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from models import UNet, Discriminator
 from utils import Horse2zebraDataset
 import os
 from zipfile import ZipFile
+from tqdm import tqdm
 
 
 
@@ -20,11 +22,33 @@ class CycleGAN(nn.Module):
         self.gen_BA = self.initialize_generator(self.dataset_name, checkpoint_name, self.device, self.file_dir, "gen_BA")
 
     
-    def train(self, n_epochs, batch_size, lr, checkpoint_name):
+    def train(self, n_epochs, batch_size, lr):
         dataloader = DataLoader(self.instantiate_dataset(self.dataset_name, self.get_transform(self.dataset_name), self.file_dir), 
                                 batch_size, True)
         disc_A = self.initialize_discriminator(self.dataset_name, self.checkpoint_name, self.device, self.file_dir, "disc_A")
         disc_B = self.initialize_discriminator(self.dataset_name, self.checkpoint_name, self.device, self.file_dir, "disc_B")
+        gen_optimizer = self.initialize_gen_optimizer(self.gen_AB, self.gen_BA, lr, self.checkpoint_name, self.file_dir, self.device)
+        disc_optimizer = self.initialize_disc_optimizer(disc_A, disc_B, lr, self.checkpoint_name, self.file_dir, self.device)
+
+        for epoch in range(n_epochs):
+            for realA, realB in tqdm(dataloader):
+                realA = realA.to(self.device)
+                realB = realB.to(self.device)
+                fakeB = self.gen_AB(realA)
+                fakeA = self.gen_BA(realB)
+
+                # update discriminator
+                disc_optimizer.zero_grad()
+                disc_loss = self.get_disc_loss(disc_A, disc_B, realA, realB, fakeA, fakeB)
+                disc_loss.backward()
+                disc_optimizer.step()
+
+                # update generator
+                gen_optimizer.zero_grad()
+                gen_loss = self.get_gen_loss()
+
+
+
 
 
     def generate(self, n_samples, checkpoint_name):
@@ -79,5 +103,27 @@ class CycleGAN(nn.Module):
         """Returns the transform object for a given dataset name"""
         if dataset_name == "horse2zebra":
             return transforms.Compose([transforms.ToTensor(), lambda x: 2*x - 1])
+
+    def initialize_disc_optimizer(self, disc_A, disc_B, lr, checkpoint_name, file_dir, device):
+        """Initializes discriminator optimizer"""
+        disc_optimizer = optim.Adam(list(disc_A.parameters()) + list(disc_B.parameters()), lr)
+        if checkpoint_name:
+            checkpoint = torch.load(os.path.join(file_dir, "checkpoints", checkpoint_name), map_location=device)
+            disc_optimizer.load_state_dict(checkpoint["disc_optimizer_state_dict"])
+        return disc_optimizer
+    
+    def initialize_gen_optimizer(self, gen_AB, gen_BA, lr, checkpoint_name, file_dir, device):
+        """Initializes generator optimizer"""
+        gen_optimizer = optim.Adam(list(gen_AB.parameters()) + list(gen_BA.parameters), lr)
+        if checkpoint_name:
+            checkpoint = torch.load(os.path.join(file_dir, "checkpoints", checkpoint), device)
+            gen_optimizer.load_state_dict(checkpoint["gen_optimizer_state_dict"])
+        return gen_optimizer
+                
+    def get_disc_loss(self, disc_A, disc_B, realA, realB, fakeA, fakeB):
+        """Returns discriminator loss"""
+        disc_loss_A = nn.functional.l1_loss(disc_A(fakeA.detach()), disc_A(realA))
+        disc_loss_B = nn.functional.l1_loss(disc_B(fakeB.detach()), disc_B(realB))
+        return disc_loss_A + disc_loss_B
 
 
