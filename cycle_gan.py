72cbbd11f6667b44895ebf08e58ac125054e5826
burak <byrkbrk@gmail.com>
2024-04-13 03:30:24 +0300

    update cyclegan class

--- cycle_gan.py
+++ cycle_gan.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch import optim
 from torchvision import transforms
+from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 from models import UNet, Discriminator
 from utils import Horse2zebraDataset
@@ -20,18 +21,20 @@ class CycleGAN(nn.Module):
         self.device = torch.device(device)
         self.gen_AB = self.initialize_generator(self.dataset_name, checkpoint_name, self.device, self.file_dir, "gen_AB")
         self.gen_BA = self.initialize_generator(self.dataset_name, checkpoint_name, self.device, self.file_dir, "gen_BA")
-
+        self.create_dirs(self.file_dir)
     
-    def train(self, n_epochs, batch_size, lr):
+    def train(self, n_epochs, batch_size, lr, criterion_name="L1", lambda_id=0.1, lambda_cycle=10):
         dataloader = DataLoader(self.instantiate_dataset(self.dataset_name, self.get_transform(self.dataset_name), self.file_dir), 
                                 batch_size, True)
         disc_A = self.initialize_discriminator(self.dataset_name, self.checkpoint_name, self.device, self.file_dir, "disc_A")
         disc_B = self.initialize_discriminator(self.dataset_name, self.checkpoint_name, self.device, self.file_dir, "disc_B")
         gen_optimizer = self.initialize_gen_optimizer(self.gen_AB, self.gen_BA, lr, self.checkpoint_name, self.file_dir, self.device)
         disc_optimizer = self.initialize_disc_optimizer(disc_A, disc_B, lr, self.checkpoint_name, self.file_dir, self.device)
+        criterion = self.instantiate_criterion(criterion_name)
 
+        avg_disc_loss = avg_gen_loss = 0
         for epoch in range(n_epochs):
-            for realA, realB in tqdm(dataloader):
+            for realA, realB in tqdm(dataloader, desc=f"Epoch {epoch}"):
                 realA = realA.to(self.device)
                 realB = realB.to(self.device)
                 fakeB = self.gen_AB(realA)
@@ -39,17 +42,23 @@ class CycleGAN(nn.Module):
 
                 # update discriminator
                 disc_optimizer.zero_grad()
-                disc_loss = self.get_disc_loss(disc_A, disc_B, realA, realB, fakeA, fakeB)
+                disc_loss = self.get_disc_loss(disc_A, disc_B, realA, realB, fakeA, fakeB, criterion)
                 disc_loss.backward()
                 disc_optimizer.step()
 
                 # update generator
                 gen_optimizer.zero_grad()
-                gen_loss = self.get_gen_loss()
+                gen_loss = self.get_gen_loss(self.gen_AB, self.gen_BA, disc_A, disc_B, realA, realB, fakeA, fakeB,
+                                             criterion, criterion, criterion, lambda_id, lambda_cycle)
+                gen_loss.backward()
+                gen_optimizer.step()
 
-
-
-
+                avg_disc_loss += disc_loss.item()/len(dataloader)
+                avg_gen_loss += gen_loss.item()/len(dataloader)
+            print(f"Epoch: {epoch}, Discriminator loss: {avg_disc_loss}, Generator loss {avg_gen_loss}")
+            self.save_tensor_images(realA, fakeA, realB, fakeB, epoch, self.file_dir)
+            avg_disc_loss = avg_gen_loss = 0
+            
 
     def generate(self, n_samples, checkpoint_name):
         pass
@@ -89,6 +98,7 @@ class CycleGAN(nn.Module):
             return Horse2zebraDataset(os.path.join(file_dir, "datasets", dataset_name), transform, train)
 
     def unzip_dataset(self, dataset_name, file_dir):
+        """Unzip dataset for given dataset name"""
         if dataset_name == "horse2zebra":
             extract_to = os.path.join(file_dir, "datasets", dataset_name)
             if os.path.exists(extract_to):
@@ -120,10 +130,65 @@ class CycleGAN(nn.Module):
             gen_optimizer.load_state_dict(checkpoint["gen_optimizer_state_dict"])
         return gen_optimizer
                 
-    def get_disc_loss(self, disc_A, disc_B, realA, realB, fakeA, fakeB):
+    def get_disc_loss(self, disc_A, disc_B, realA, realB, fakeA, fakeB, criterion):
         """Returns discriminator loss"""
-        disc_loss_A = nn.functional.l1_loss(disc_A(fakeA.detach()), disc_A(realA))
-        disc_loss_B = nn.functional.l1_loss(disc_B(fakeB.detach()), disc_B(realB))
+        disc_loss_A = self.get_disc_X_loss(disc_A, realA, fakeA, criterion)
+        disc_loss_B = self.get_disc_X_loss(disc_B, realB, fakeB, criterion)
         return disc_loss_A + disc_loss_B
+    
+    def get_disc_X_loss(self, disc_X, realX, fakeX, criterion):
+        """Returns discriminator X loss"""
+        pred_real = disc_X(realX)
+        pred_fake = disc_X(fakeX)
+        return 1/2*(criterion(pred_real, torch.ones_like(pred_real)) + criterion(pred_fake, torch.zeros_like(pred_fake)))
+    
+    def get_gen_loss(self, gen_AB, gen_BA, disc_A, disc_B, realA, realB, fakeA, fakeB, 
+                     id_criterion, cycle_criterion, adv_criterion, lambda_id, lambda_cycle):
+        """Returns generator loss"""
+        # Generator_AB loss
+        id_loss_AB = self.get_id_loss(gen_AB, realB, id_criterion, lambda_id)
+        cycle_loss_AB = self.get_cycle_loss(gen_AB, fakeB, realA, cycle_criterion)
+        adv_loss_AB = self.get_adv_loss(fakeB, disc_B, adv_criterion)
+        gen_loss_AB = id_loss_AB % cycle_loss_AB + adv_loss_AB
 
+        # Generator BA loss
+        id_loss_BA = self.get_id_loss(gen_BA, realA, id_criterion, lambda_id)
+        cycle_loss_BA = self.get_cycle_loss(gen_BA, fakeB, realA, cycle_criterion, lambda_cycle)
+        adv_loss_BA = self.get_adv_loss(fakeA, disc_A, adv_criterion)
+        gen_loss_BA = id_loss_BA + cycle_loss_BA + adv_loss_BA
+        return 1/2*(gen_loss_AB + gen_loss_BA)
+
+    def get_id_loss(self, gen_XY, realY, criterion, lambda_id):
+        """Returns identity loss"""
+        return lambda_id*criterion(gen_XY(realY), realY)
+    
+    def get_cycle_loss(self, gen_YX, fakeY, realX, criterion, lambda_cycle):
+        """Returns cycle loss"""
+        return lambda_cycle*criterion(gen_YX(fakeY), realX)
+    
+    def get_adv_loss(self, fakeY, disc_Y, criterion):
+        """Returns adversarial loss"""
+        pred = disc_Y(fakeY)
+        return criterion(pred, torch.ones_like(pred))
+    
+    def instantiate_criterion(self, criterion_name="L1"):
+        """Returns instantiated criterion"""
+        assert criterion_name in {"L1", "mse"}, "Unknown criterion name"
+
+        if criterion_name == "L1":
+            return nn.L1Loss()
+        elif criterion_name == "mse":
+            return nn.MSELoss()
+    
+    def save_tensor_images(self, realA, fakeA, realB, fakeB, epoch, file_dir):
+        """Save given tensor images into saved-images directory"""
+        save_image([realA, fakeA, realB, fakeB], 
+                   os.path.join(file_dir, "saved_images", f"realA_fakeA_realB_fakeB_{epoch}.jpeg"), 
+                   nrow=len(realA))
+
+    def create_dirs(self, file_dir):
+        """Create directories used in training and inferencing"""
+        dir_names = ["checkpoints", "saved-images"]
+        for dir_name in dir_names:
+            os.makedirs(os.path.join(file_dir, dir_name), exist_ok=True)
 
