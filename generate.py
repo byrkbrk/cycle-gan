24277e90652769b2513ec7cdb49a423ef2596183
burak <byrkbrk@gmail.com>
2024-04-27 21:48:32 +0300

    add dataset-name argument into parser

--- generate.py
+++ generate.py
@@ -7,8 +7,10 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Generate images from Cycle GAN")
     parser.add_argument("checkpoint_name", type=str, default=None, help="Checkpoint name to use for inference")
     parser.add_argument("--allow-checkpoint-download", type=bool, default=False, help="Downloads pretrained checkpoint if True")
+    parser.add_argument("--dataset-name", type=str, default=None, help="Dataset name for inference, required when 'allow-checkpoint-download' is True")
     args = parser.parse_args()
 
-    cycle_gan = CycleGAN(args.checkpoint_name, allow_checkpoint_download=args.allow_checkpoint_download)
+    cycle_gan = CycleGAN(args.checkpoint_name, dataset_name=args.dataset_name,
+                         allow_checkpoint_download=args.allow_checkpoint_download)
     cycle_gan.generate("AB")
     cycle_gan.generate("BA")
