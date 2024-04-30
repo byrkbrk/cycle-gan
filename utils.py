33c1a80b7c63c70f632c18ca39827aeb01f4d61a
burak <byrkbrk@gmail.com>
2024-04-30 06:14:58 +0300

    add Monet2photoDataset

--- utils.py
+++ utils.py
@@ -47,24 +47,83 @@ class Horse2zebraDataset(Dataset):
         return sorted(files, key=f)
 
 
+class Monet2photoDataset(Dataset):
+    """Monet2photo dataset"""
+    base_folder = dataset_name = "monet2photo"
+    def __init__(self, root, transform, train, download):
+        if download:
+            download_dataset(self.dataset_name, root)
+        self.unzip_dataset(self.base_folder, root)
+        
+        if train:
+            self.dataset_pathA = os.path.join(root, self.base_folder, "trainA")
+            self.dataset_pathB = os.path.join(root, self.base_folder, "trainB")
+            self.image_pathsA = [os.path.join(self.dataset_pathA, file) for file in os.listdir(self.dataset_pathA)]
+            self.image_pathsB = [os.path.join(self.dataset_pathB, file) for file in os.listdir(self.dataset_pathB)]
+        else:
+            self.dataset_pathA = os.path.join(root, self.base_folder, "testA")
+            self.dataset_pathB = os.path.join(root, self.base_folder, "testB")
+            self.image_pathsA = [os.path.join(self.dataset_pathA, file) for file in self.sort_files(os.listdir(self.dataset_pathA))]
+            self.image_pathsB = [os.path.join(self.dataset_pathB, file) for file in self.sort_files(os.listdir(self.dataset_pathB))]
+        self.transform = transform
+
+    def __getitem__(self, index):
+        if index < len(self.image_pathsA): imageA = self.read_image(self.image_pathsA[index])
+        else: imageA = self.read_image(self.image_pathsA[torch.randint(len(self.image_pathsA), (1, ))])
+
+        if index < len(self.image_pathsB): imageB = self.read_image(self.image_pathsB[index])
+        else: imageB = self.read_image(self.image_pathsB[torch.randint(len(self.image_pathsB), (1, ))])
+        return self.transform(imageA), self.transform(imageB)
+
+    def __len__(self):
+        return max(len(self.image_pathsA), len(self.image_pathsB))
+    
+    def read_image(self, img_path):
+        return Image.open(img_path)
+    
+    def sort_files(self, files):
+        """Sorts based on file indices for a given list of files"""
+        return sorted(files)
+    
+    def unzip_dataset(self, base_folder, root):
+        """Unzip monet2photo.zip dataset"""
+        if os.path.exists(os.path.join(root, base_folder)):
+            print(f"Directory {os.path.join(root, base_folder)} already exists. No operation done")
+            return
+        file_names = ["monet2photo.zip"]
+        extract_to = os.path.join(root, "")
+        for file_name in file_names:
+            with ZipFile(os.path.join(root, file_name), "r") as zip_file:
+                    zip_file.extractall(extract_to)    
+        
+
 def unzip_dataset(dataset_name, base_folder, root):
         """Unzip dataset for given dataset name"""
         if dataset_name == "horse2zebra":
-            extract_to = os.path.join(root, base_folder)
-            if os.path.exists(extract_to):
+            file_names =  ["horse2zebraA.zip", "horse2zebraB.zip"]
+        elif dataset_name == "monet2photo":
+            file_names = ["monet2photo.zip"]
+        else:
+            raise ValueError(f"Undefined dataset name: {dataset_name}")
+        
+        extract_to = os.path.join(root, base_folder)
+        print(dataset_name, extract_to)
+        if os.path.exists(extract_to):
                 print(f"Directory {extract_to} already exists. No operation done")
-            else:
-                os.mkdir(extract_to)
-                for file_name in ["horse2zebraA.zip", "horse2zebraB.zip"]:
-                    with ZipFile(os.path.join(root, file_name), "r") as zip_file:
-                        zip_file.extractall(extract_to)
+                return
+        os.mkdir(extract_to)
+        
+        for file_name in file_names:
+                with ZipFile(os.path.join(root, file_name), "r") as zip_file:
+                    zip_file.extractall(extract_to)
 
 
-def download_dataset(dataset_name, root, base_folder, url=None):
-    """Downloads dataset into root/base_folder directory"""
+
+def download_dataset(dataset_name, root, url=None):
+    """Downloads dataset into root/ directory"""
     if url is None:
         url = f"https://efrosgans.eecs.berkeley.edu/cyclegan/datasets/{dataset_name}.zip"
-    file_path = os.path.join(root, base_folder, f"{dataset_name}.zip")
+    file_path = os.path.join(root, f"{dataset_name}.zip")
     
     if os.path.exists(file_path):
         print(f"File {file_path} already exists. No operation done!")
@@ -150,3 +209,4 @@ if __name__ == "__main__":
     # download_dataset(dataset_name, file_dir)
 
     dataset = Horse2zebraDataset("datasets", lambda x: x, True)
+    dataset = Monet2photoDataset("datasets", lambda x: x, True, False)
